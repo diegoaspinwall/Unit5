b1ad80150e9696612bba71b572a540d6daa449f7
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-15 14:00:01 -0500

    Updated from Brython Server: 11/15/2017 2:00:01 PM

--- stat.py
+++ stat.py
@@ -1,1 +1,15 @@
+#Diego Aspinwall
+#10-15-17
+#stat.py
 
+print('Enter a list of numbers')
+print('Enter "q" when done')
+
+list = []
+
+while True:
+    q = input('>')
+    if q == 'q':
+        break
+    else:
+        list.append(q)
