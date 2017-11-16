d07c7cc318c52a825920898cb9ab8360d7baf4c2
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-16 14:14:32 -0500

    Updated from Brython Server: 11/16/2017 2:14:31 PM

--- warmup13.py
+++ warmup13.py
@@ -1,1 +1,14 @@
+#Diego Aspinwall
+#10-16-17
+#warmup13.py
 
+from random import randint
+
+for i in range(0,20):
+    append.list = randint(1,100)
+
+list = []
+
+print('Min:',min(list))
+print('Max:',max(list))
+print('Sum:',sum(list))
