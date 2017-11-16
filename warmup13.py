af5c1381e42a38717ca4319377a81101331549d7
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-16 14:29:15 -0500

    Updated from Brython Server: 11/16/2017 2:29:14 PM

--- warmup13.py
+++ warmup13.py
@@ -5,19 +5,6 @@
 from random import randint
 
 num = []
-i = 1
-while i<21:
-    num.append(randint(0,50))
-    i+=1
-print(min(num))
-print(max(num))
-print(sum(num))
-
-
-'''
-from random import randint
-
-num = []
 
 for i in range(0,20):
     num.append(randint(1,100))
@@ -25,4 +12,3 @@ for i in range(0,20):
 print('Min:',min(num))
 print('Max:',max(num))
 print('Sum:',sum(num))
-'''
