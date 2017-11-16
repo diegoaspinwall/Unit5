99ecbcac1506b9f7064ab5b8c0811b6e729dee58
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-16 14:37:44 -0500

    Updated from Brython Server: 11/16/2017 2:37:43 PM

--- toDoList.py
+++ toDoList.py
@@ -1,1 +1,17 @@
+#Diego Aspinwall
+#10-16-17
+#toDoList.py
 
+dolist = []
+
+print('Valid commands: add, delete, print, quit')
+while True:
+    qsearch = input('>')
+    if 'quit' in qsearch:
+        break
+    elif qsearch == 'print':
+        print(dolist)'''
+        
+    
+        list.append(int(qsearch))
+'''
