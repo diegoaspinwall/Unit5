54038b6fe0a0626260077912340f5e700dee4329
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-17 14:40:24 -0500

    Updated from Brython Server: 11/17/2017 2:40:25 PM

--- snow.py
+++ snow.py
@@ -1,3 +1,13 @@
 #Diego Aspinwall
 #11-17-17
 #snow.py
+
+COLS = 20
+SIZE = 10
+
+white = Color(0xffffff,1)
+backg = Color(0x000000,1)
+backgOutline = LineStyle(1,backg)
+whiteOutline = LineStyle(1,white)
+
+redRectangle = RectangleAsset(200,100,blackOutline,red)
