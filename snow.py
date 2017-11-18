a9201414d86793b207d36cbe1ed40af8b3faa4f2
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-18 17:20:01 -0500

    Updated from Brython Server: 11/18/2017 5:20:01 PM

--- snow.py
+++ snow.py
@@ -6,7 +6,6 @@ from ggame import *
 
 def spriteflake():
     Sprite(snowBlock, (SIZE*randint(0,COLS),SIZE*randint(0,COLS)))
-    #fallflake.x = SIZE*randint(0,COLS)
     data['frames'] = 0
 
 def step():
@@ -16,17 +15,20 @@ def step():
 
 if __name__ == '__main__':
     
+    data = {}
+    data['frames'] = 0
+    
     COLS = 20
     SIZE = 15
     
-    white = Color(0xffffff,1)
+    white = Color(0xfff00f,1)
     backg = Color(0x000000,1)
     backgOutline = LineStyle(1,backg)
     whiteOutline = LineStyle(1,white)
     
     backgRectangle = RectangleAsset(COLS*SIZE,COLS*SIZE,backgOutline,backg)
     snowBlock = RectangleAsset(SIZE,SIZE,whiteOutline,white)
-    Sprite(snowBlock)
     
-    Sprite(backgRectangle)
+    #Sprite(backgRectangle)
+    Sprite(snowBlock)
     App().run(step)
