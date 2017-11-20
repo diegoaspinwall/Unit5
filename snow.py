ff139d83e53ea4f8858b1b6b37090082723093a5
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-20 14:42:19 -0500

    Updated from Brython Server: 11/20/2017 2:42:19 PM

--- snow.py
+++ snow.py
@@ -6,14 +6,16 @@ from ggame import *
 from random import randint
 
 def spriteflake():
-    fallflake = Sprite(snowBlock, (SIZE*randint(0,COLS-1),0))
+    data['flakes'].append(Sprite(snowBlock, (SIZE*randint(0,COLS-1),0)))
     data['frames'] = 0
+    
 
 def step():
     data['frames'] += 1
     if data['frames'] == 20:
         spriteflake()
-    #fallflake.y += 1
+    for flake in data['flakes']:
+        flake.y += 1
 
 
 if __name__ == '__main__':
@@ -35,3 +37,13 @@ if __name__ == '__main__':
     
     Sprite(backgRectangle)
     App().run(step)
+
+'''
+  for ant in data['antList']:
+        dx = randint(-4,3)
+        dy = randint(-4,3)
+        if ant.x+dx>0 and ant.x+dx<WIDTH:
+            ant.x += dx
+        if ant.y+dy>0 and ant.y+dy<HEIGHT:
+            ant.y += dy
+'''
