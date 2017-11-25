d4012ee1303a3c8ed684da88d516ddbb0f0ba357
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-24 23:46:35 -0500

    Updated from Brython Server: 11/24/2017 11:46:41 PM

--- snow.py
+++ snow.py
@@ -26,15 +26,15 @@ def step():
 
 if __name__ == '__main__':
     
-    COLS = 10
-    SIZE = 30
-    
     data = {}
     data['frames'] = 0
     data['flakes'] = []
     data['fallpos'] = []
-    for i in range(0,COLS):
+    for i in range(0,100):
         data['fallpos'].append(0)
+    
+    COLS = 100
+    SIZE = 3
     
     white = Color(0x00ffff,1)
     backg = Color(0x000000,1)
