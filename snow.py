24ef80cc89917e1b0c5ad7eddac6c9b59a30cbbb
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-25 00:12:25 -0500

    Updated from Brython Server: 11/25/2017 12:12:31 AM

--- snow.py
+++ snow.py
@@ -10,7 +10,7 @@ def spriteflake():
     data['flakes'].append(Sprite(snowBlock, (SIZE*xpos,-SIZE)))
     data['fallpos'][xpos] += 1
     data['stopflake'].append(data['fallpos'][xpos])
-    print(data['fallpos'])
+    print(data['stopflake'])
     
 
 def step():
