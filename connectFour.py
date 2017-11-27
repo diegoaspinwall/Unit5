d35e9350709141a1105cb8aa07e79ad2c7c529ff
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-27 10:19:35 -0500

    Updated from Brython Server: 11/27/2017 10:19:36 AM

--- connectFour.py
+++ connectFour.py
@@ -4,3 +4,10 @@
 
 from ggame import *
 
+if __name__ == '__main__':
+    
+    white = Color(0xffffff,1)
+    red = Color(0xff0000,1)
+    blue = Color(0x0000ff,1)
+    black = Color(0x000000,1)
+    blackOutline = LineStyle(1,black)
