fbae406c4b9c3f85638a0a260e23bcfcf26a982d
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-27 10:22:06 -0500

    Updated from Brython Server: 11/27/2017 10:22:07 AM

--- connectFour.py
+++ connectFour.py
@@ -4,6 +4,12 @@
 
 from ggame import *
 
+def printBoard:
+    for row in range(0,3):
+        for col in range(0,3):
+            print(board[row][col],' ',end = '')
+        print()
+
 if __name__ == '__main__':
     
     white = Color(0xffffff,1)
@@ -11,3 +17,7 @@ if __name__ == '__main__':
     blue = Color(0x0000ff,1)
     black = Color(0x000000,1)
     blackOutline = LineStyle(1,black)
+    
+    hole = CircleAsset(50,blackOutline,white)
+    
+    App().run()
