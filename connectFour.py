5e2f87fa61ccf61f23f1d62175aef44a7e30f5a1
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-27 10:23:13 -0500

    Updated from Brython Server: 11/27/2017 10:23:14 AM

--- connectFour.py
+++ connectFour.py
@@ -7,8 +7,7 @@ from ggame import *
 def printBoard:
     for row in range(0,3):
         for col in range(0,3):
-            print(board[row][col],' ',end = '')
-        print()
+            Sprite(hole, (50*row+10, 50*col+10))
 
 if __name__ == '__main__':
     
