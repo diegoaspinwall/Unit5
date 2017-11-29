38f452e2a00abb49e26a806730a89c3aa5812dc3
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-29 13:57:02 -0500

    Updated from Brython Server: 11/29/2017 1:57:02 PM

--- cocktailSort.py
+++ cocktailSort.py
@@ -27,25 +27,7 @@ def mySort(A):
     return A
 '''
 '''
-procedure siftDown(a, start, end) is
-    root ← start
 
-    while iLeftChild(root) ≤ end do    (While the root has at least one child)
-        child ← iLeftChild(root)   (Left child of root)
-        swap ← root                (Keeps track of child to swap with)
-
-        if a[swap] < a[child]
-            swap ← child
-        (If there is a right child and that child is greater)
-        if child+1 ≤ end and a[swap] < a[child+1]
-            swap ← child + 1
-        if swap = root
-            (The root holds the largest element. Since we assume the heaps rooted at the
-             children are valid, this means that we are done.)
-            return
-        else
-            swap(a[root], a[swap])
-            root ← swap            (repeat to continue sifting down the child now)
 '''
 
 #heapsort
@@ -73,7 +55,15 @@ def siftDown(a, start, end):
         swap = root
         if a[swap] < a[child]:
             swap = child
-            
+        if child+1 <= end and a[swap] < a[child+1]:
+            swap = child+1
+        if swap == root:
+            return
+        else:
+            a[root], a[swap] = a[swap], a[root]
+            root = swap
+
+
 
 
 if __name__ == '__main__':
