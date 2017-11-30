606e9b31c9390a32214a4aa10bbba1b65d5762f2
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-30 14:27:04 -0500

    Updated from Brython Server: 11/30/2017 2:27:04 PM

--- cocktailSort.py
+++ cocktailSort.py
@@ -8,7 +8,6 @@ from math import log
 
 N = 100 #how many numbers will be sorted
 
-'''
 def mySort(A):
     swapped = True
     while swapped:
@@ -25,51 +24,6 @@ def mySort(A):
                 A[i], A[i+1] = A[i+1], A[i]
                 swapped = True
     return A
-'''
-'''
-
-'''
-
-#heapsort
-def iParent(i):
-    return int((i-1)/2)
-def iLeftChild(i):
-    return (2*i + 1)
-def iRightChild(i):
-    return (2*i + 2)
-
-def heapsort(a, count):
-    heapify(a,count)
-    end = count-1
-    while end>0:
-        a[end], a[0] = a[0], a[end]
-        end = end-1
-        siftDown(a,0,end)
-    return a
-
-def heapify(a,count):
-    start = iParent(count-1)
-    while start >= 0:
-        siftDown(a,start,count-1)
-        start = start-1
-
-def siftDown(a, start, end):
-    root = start
-    while iLeftChild(root) <= end:
-        child = iLeftChild(root)
-        swap = root
-        if a[swap] < a[child]:
-            swap = child
-        if child+1 <= end and a[swap] < a[child+1]:
-            swap = child+1
-        if swap == root:
-            return
-        else:
-            a[root], a[swap] = a[swap], a[root]
-            root = swap
-
-
-
 
 if __name__ == '__main__':
     
@@ -82,7 +36,7 @@ if __name__ == '__main__':
     
     #time how long your sort takes
     t1 = time()
-    numbers = heapsort(numbers, N)
+    numbers = mySort(numbers)
     t2 = time()
        
     #print whether the sort worked or not
