f46189a1a165b4cf28f627b360083bb189037400
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-11-30 14:12:35 -0500

    Updated from Brython Server: 11/30/2017 2:12:35 PM

--- cocktailSort.py
+++ cocktailSort.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     
     #time how long your sort takes
     t1 = time()
-    numbers = mySort(numbers)
+    numbers = heapsort(numbers, N)
     t2 = time()
        
     #print whether the sort worked or not
