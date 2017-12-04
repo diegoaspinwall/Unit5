52be60a36902e7258501860ba5f363d251c0e59d
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-04 14:22:41 -0500

    Updated from Brython Server: 12/4/2017 2:22:40 PM

--- quiz5.py
+++ quiz5.py
@@ -10,11 +10,11 @@ def rand5():
         nums.append(randint(1,100))
     return nums
 
-def lastElement(list):
-    return list[-1]
+def lastElement(aist):
+    return aist[-1]
 
-def wordLengths():
-    
+def wordLengths(bist):
+    return bist[0]
 
 print(rand5())
 print(lastElement(['cat','dog','rat']))
