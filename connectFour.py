72894cd327818278b099d4286af4b726064671d3
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2018-01-13 11:45:24 -0500

    Updated from Brython Server: 1/13/2018 11:45:24 AM

--- connectFour.py
+++ connectFour.py
@@ -5,29 +5,20 @@
 from ggame import *
 
 def placePiece(event):
-    #Sprite(redfill, (event.x,event.y))
+    Sprite(redfill, (event.x,event.y))
     print('R =',((event.x-CIRCX)**2+(event.y-CIRCY)**2)**.5)
     print(event.x,event.y)
     if ((event.x-CIRCX)**2+(event.y-CIRCY)**2)**.5 <= CIRCR:
         print('Inside')
     else:
         print('Outside')
-        Sprite(LineAsset(event.x-CIRCX,event.y-CIRCY,blackOutline), (CIRCX,CIRCY))
 
 def printBoard():
-    Sprite(testhole, (CIRCX,CIRCY))
-    '''
     for row in range(0,6):
         for col in range(0,7):
             Sprite(hole, (90*col+50, 90*row+50))
-    '''
 
 if __name__ == '__main__':
-    
-    CIRCX = 300
-    CIRCY = 200
-    CIRCR = 200
-    
     white = Color(0xffffff,1)
     red = Color(0xff0000,1)
     blue = Color(0x0000ff,1)
